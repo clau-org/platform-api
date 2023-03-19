25076fa8f1a7b8848aac68d399744a282adc27b6
Francisco Ramos <fro.profesional@gmail.com>
2023-03-19 11:22:52 -0700

    Added name and date to message

--- src/routers/pusher.ts
+++ src/routers/pusher.ts
@@ -1,6 +1,7 @@
 import { Router } from "oak";
 import { logger } from "../utils/logger.ts";
 import { pusher } from "../channels/pusher.ts";
+import { getFakeName } from "../tests/faker.ts";
 
 const pusherRouter = new Router();
 
@@ -8,8 +9,22 @@ pusherRouter.all("/pusher", async (ctx) => {
   let pResponse = null;
 
   try {
+    let name = await getFakeName();
+
+    const date = new Date();
+    const options: any = {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+      fractionalSecondDigits: 3,
+    };
+    const mexicanDate = date.toLocaleString("es-MX", options);
+
     pResponse = await pusher.trigger("chat-room", "new-message", {
-      message: "Ey, it seems the implementation works! :) - DENO!",
+      message: `Hi from ${name} at ${mexicanDate}`,
     });
 
     pResponse = await pResponse.json();
